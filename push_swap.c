fcb09b823f8f80d26d504a64faf6eb496bdf0da6
RomanAlexandroff <r.aleksandroff@gmail.com>
2026-03-14 13:42:44 +0100

    Rename nodes variable

--- push_swap.c
+++ push_swap.c
@@ -16,22 +16,22 @@ int	main(int argc, char **argv)
 {
 	t_node	*a;
 	t_node	*b;
-    int     nodes_count;
+    int     nodes_quantity;
 
 	a = NULL;
 	b = NULL;
-    nodes_count = 0;
+    nodes_quantity = 0;
 	if (argc <= 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
 		argv = extract_values(argv[1]);
 	create_stack_safely(&a, argv + 1, argc);
-    nodes_count = get_stack_length(a);
+    nodes_quantity = get_stack_length(a);
 	if (!is_sorted(a))
 	{
-		if (nodes_count == 2)
+		if (nodes_quantity == 2)
 			swap_a(&a); 
-		else if (nodes_count == 3)
+		else if (nodes_quantity == 3)
 			sort_three(&a);
 		else
 			sort_many(&a, &b);
