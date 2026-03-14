310952c7148bb2ac3a7055a26dc49749c2e6aa71
RomanAlexandroff <r.aleksandroff@gmail.com>
2026-03-14 13:45:27 +0100

    Improve variable naming

--- push_swap.c
+++ push_swap.c
@@ -16,22 +16,22 @@ int	main(int argc, char **argv)
 {
 	t_node	*a;
 	t_node	*b;
-    int     nodes_count;
+    int     nodes_num;
 
 	a = NULL;
 	b = NULL;
-    nodes_count = 0;
+    nodes_num = 0;
 	if (argc <= 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
 		argv = extract_values(argv[1]);
 	create_stack_safely(&a, argv + 1, argc);
-    nodes_count = get_stack_length(a);
+    nodes_num = get_stack_length(a);
 	if (!is_sorted(a))
 	{
-		if (nodes_count == 2)
+		if (nodes_num == 2)
 			swap_a(&a); 
-		else if (nodes_count == 3)
+		else if (nodes_num == 3)
 			sort_three(&a);
 		else
 			sort_many(&a, &b);
